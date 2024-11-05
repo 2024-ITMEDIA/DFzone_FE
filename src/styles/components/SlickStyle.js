f42dfedb3bd270139a4480aaa5db97e445190669
minji0824 <kmj0824mj@gmail.com>
2024-11-06 04:29:05 +0900

    Feat: Slick 컴포넌트 구현

--- src/styles/components/SlickStyle.js
+++ src/styles/components/SlickStyle.js
@@ -1,4 +1,41 @@
 import styled from "styled-components";
-import palette from "../../lib/colorPalette";
+import rightarrow from "../../img/icon_right.png";
+import leftarrow from "../../img/icon_left.png";
 
-export const Slick = styled.div``;
+export const NextTo = styled.div`
+  background-image: url(${rightarrow});
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 20px;
+  width: 20px;
+`;
+
+export const Prev = styled.div`
+  background-image: url(${leftarrow});
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 20px;
+  width: 20px;
+`;
+
+export const SliderContainer = styled.div`
+  margin: 0px auto;
+  overflow: visible;
+
+  .slick-prev:before,
+  .slick-next:before {
+    display: none;
+  }
+
+  .slick-slide {
+    opacity: 0.5;
+    transition: opacity 0.3s ease;
+    transform: scale(0.8); /* 기본 크기 */
+  }
+
+  .slick-center {
+    opacity: 1; /* 중앙 슬라이드는 불투명 */
+    transform: scale(1); /* 중앙 슬라이드는 크기 증가 */
+    z-index: 1; /* 중앙 슬라이드는 최상위 z-index */
+  }
+`;
