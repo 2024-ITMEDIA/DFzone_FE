89f99af5d0caf5e9b79f6063b4c306f26b7501bf
mogg22 <ahrthdus1@gmail.com>
2024-11-09 16:22:32 +0900

    Fix : 스크롤 애니메이션 상태 관리 및 퍼블리싱 최종

--- src/pages/Invite.jsx
+++ src/pages/Invite.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as I from "../styles/InviteStyle";
 import logo from "../img/text_logo.png";
 import date from "../img/text_date.png";
@@ -18,120 +18,137 @@ import DateBox from '../components/DateBox';
 import MapBox from '../components/MapBox';
 import AboutUs from '../components/AboutUs';
 import Slick from '../components/Slick';
+import Footer from '../components/Footer';
 
 function Invite() {
+    const [animation, setAnimation] = useState(false);
+
     useEffect(() => {
-        const scrollAnimation = () => {
-            const scrollY = window.scrollY * 0.5 ;
-            console.log(scrollY); // contents 요소 애니메이션 타이밍 설정 후에 삭제
-
-            // animated opacity
-            const animatedElements = document.querySelectorAll(".animated");
-            animatedElements.forEach((el) => {
-                const offset = el.offsetTop;
-                const speed = parseFloat(el.getAttribute("data-speed")) || 1;
-                
-                el.style.transform = `translateY(-${scrollY * speed }px)`;
-                el.style.opacity = Math.max(1 - (scrollY * 4) / offset, 0);
-            });
-
-            // Contents opacity
-            const contentElements = document.querySelectorAll("#contents .fade-on-scroll");
-            contentElements.forEach((el, index) => {
-                let fadeStart, fadeEnd;
-
-                // cotents 요소 애니메이션 인덱스 및 타이밍 설정 작업 필요
-                if (index < 5) {
-                    fadeStart = 310 + index * 20;
-                    fadeEnd = fadeStart + 5;
-                } else if (index >= 5 && index <10) {
-                    fadeStart = 470 + (index - 5) * 20;
-                    fadeEnd = fadeStart + 5;
+        const mediaQuery = window.matchMedia('(hover: hover) and (pointer: fine)');
+        if (mediaQuery.matches) {
+            setAnimation(mediaQuery.matches);
+        } else {
+            setAnimation(mediaQuery.matches);
+        }
+        
+        if (!mediaQuery.matches) {
+            const scrollAnimation = () => {
+                const scrollY = window.scrollY * 0.5 ;
+    
+                // animated opacity
+                const animatedElements = document.querySelectorAll(".animated");
+                animatedElements.forEach((el) => {
+                    const offset = el.offsetTop;
+                    const speed = parseFloat(el.getAttribute("data-speed")) || 1;
+                    
+                    el.style.transform = `translateY(-${scrollY * speed }px)`;
+                    el.style.opacity = Math.max(1 - (scrollY * 4) / offset, 0);
+                });
+    
+                // Contents opacity
+                const contentElements = document.querySelectorAll("#contents .fade-on-scroll");
+                contentElements.forEach((el, index) => {
+                    let fadeStart, fadeEnd;
+    
+                    // cotents 요소 애니메이션 인덱스 및 타이밍 설정 작업 필요
+                    if (index < 5) {
+                        fadeStart = 310 + index * 20;
+                        fadeEnd = fadeStart + 5;
+                    } else if (index >= 5 && index <10) {
+                        fadeStart = 500 + (index - 5) * 20;
+                        fadeEnd = fadeStart + 5;
+                    } else if (index >= 10 && index <13) {
+                        fadeStart = 750 + (index - 13) * 20;
+                        fadeEnd = fadeStart + 5;
+                    } else if (index >= 13 && index <14) {
+                        fadeStart = 820 + (index - 14) * 20;
+                        fadeEnd = fadeStart + 5;
+                    } else {
+                        fadeStart = 1000 + (index - 20) * 20;
+                        fadeEnd = fadeStart + 5;
+                    }
+    
+                    if (scrollY >= fadeStart && scrollY <= fadeEnd) {
+                        const opacity = 1 - (scrollY - fadeStart) / (fadeEnd - fadeStart);
+                        el.style.opacity = opacity;
+                    } else if (scrollY > fadeEnd) {
+                        el.style.opacity = 0;
+                    } else {
+                        el.style.opacity = 1;
+                    }
+    
+                    el.style.transition = "opacity 1s ease";
+                });
+    
+                // Title animation
+                const title = document.querySelector("#title");
+                if (scrollY < 200) {
+                    title.style.position = "fixed";
+                    title.style.transformOrigin = "top left";
+                    title.style.fontSize = `${10 - scrollY * 0.015}px`;
+                    title.style.top = `${46 - scrollY * 0.135}px`;
                 } else {
-                    fadeStart = 1000 + (index - 13) * 20;
-                    fadeEnd = fadeStart + 5;
+                    title.style.position = "fixed";
+                    title.style.top = "19px";
+                    title.style.fontSize = "7px";
                 }
-
-                if (scrollY >= fadeStart && scrollY <= fadeEnd) {
-                    const opacity = 1 - (scrollY - fadeStart) / (fadeEnd - fadeStart);
-                    el.style.opacity = opacity;
-                } else if (scrollY > fadeEnd) {
-                    el.style.opacity = 0;
+    
+                // Logo animation
+                const logo = document.querySelector("#logo");
+                if (scrollY < 200) {
+                    title.style.position = "fixed";
+                    logo.style.transformOrigin = "top left";
+                    logo.style.width = `${306 - scrollY * 0.64}px`;
+                    logo.style.height = `${176 - scrollY * 0.365}px`;
+                    logo.style.top = `${68 - scrollY * 0.14}px`;
                 } else {
-                    el.style.opacity = 1;
+                    logo.style.position = "fixed";
+                    logo.style.top = "40px";
+                    logo.style.width = "178px";
+                    logo.style.height = "103px";
                 }
-
-                el.style.transition = "opacity 1s ease";
-            });
-
-            // Title animation
-            const title = document.querySelector("#title");
-            if (scrollY < 200) {
-                title.style.position = "fixed";
-                title.style.transformOrigin = "top left";
-                title.style.fontSize = `${10 - scrollY * 0.015}px`;
-                title.style.top = `${46 - scrollY * 0.135}px`;
-            } else {
-                title.style.position = "fixed";
-                title.style.top = "19px";
-                title.style.fontSize = "7px";
-            }
-
-            // Logo animation
-            const logo = document.querySelector("#logo");
-            if (scrollY < 200) {
-                title.style.position = "fixed";
-                logo.style.transformOrigin = "top left";
-                logo.style.width = `${306 - scrollY * 0.64}px`;
-                logo.style.height = `${176 - scrollY * 0.365}px`;
-                logo.style.top = `${68 - scrollY * 0.14}px`;
-            } else {
-                logo.style.position = "fixed";
-                logo.style.top = "40px";
-                logo.style.width = "178px";
-                logo.style.height = "103px";
-            }
-
-            // Date animation
-            const date = document.querySelector("#date");
-            if (scrollY < 200) {
-                title.style.position = "fixed";
-                date.style.transformOrigin = "top left";
-                date.style.width = `${131 - scrollY * 0.205}px`;
-                date.style.height = `${52 - scrollY * 0.08}px`;
-                date.style.top = `${253 - scrollY * 0.51}px`;
-                date.style.left = "30px";
-            } else {
-                date.style.position = "fixed";
-                date.style.top = "151px";
-                date.style.width = "90px";
-                date.style.height = "36px";
-                date.style.left = "30px";
-            }
-
-            // Center animation
-            const center = document.querySelector("#center");
-            if (scrollY < 200) {
-                title.style.position = "fixed";
-                center.style.transformOrigin = "top left";
-                center.style.transform = `translateY(${(scrollY / 200) * 0.02}px) translateX(${scrollY * -0.12}px)`;
-                center.style.width = `${332 - scrollY * 0.655}px`;
-                center.style.height = `${324 - scrollY * 0.64}px`;
-                center.style.top = `${411 - scrollY * 1.955}px`;
-                center.style.right = `${scrollY * 0.1}px`;
-                const rightValue = 34 - (scrollY / 200) * (34 + 9);
-                center.style.right = `${rightValue}px`;
-            } else {
-                center.style.position = "fixed";
-                center.style.top = "20px";
-                center.style.width = "201px";
-                center.style.height = "196px";
-                center.style.right = "-9px";
-            }
-        };
-
-        window.addEventListener("scroll", scrollAnimation);
-        return () => window.removeEventListener("scroll", scrollAnimation);
+    
+                // Date animation
+                const date = document.querySelector("#date");
+                if (scrollY < 200) {
+                    title.style.position = "fixed";
+                    date.style.transformOrigin = "top left";
+                    date.style.width = `${131 - scrollY * 0.205}px`;
+                    date.style.height = `${52 - scrollY * 0.08}px`;
+                    date.style.top = `${253 - scrollY * 0.51}px`;
+                    date.style.left = "40px";
+                } else {
+                    date.style.position = "fixed";
+                    date.style.top = "151px";
+                    date.style.width = "90px";
+                    date.style.height = "36px";
+                    date.style.left = "40px";
+                }
+    
+                // Center animation
+                const center = document.querySelector("#center");
+                if (scrollY < 200) {
+                    title.style.position = "fixed";
+                    center.style.transformOrigin = "top left";
+                    center.style.transform = `translateY(${(scrollY / 200) * 0.02}px) translateX(${scrollY * -0.12}px)`;
+                    center.style.width = `${332 - scrollY * 0.655}px`;
+                    center.style.height = `${324 - scrollY * 0.64}px`;
+                    center.style.top = `${411 - scrollY * 1.955}px`;
+                    center.style.right = `${scrollY * 0.1}px`;
+                    const rightValue = 34 - (scrollY / 200) * (34 + 9);
+                    center.style.right = `${rightValue}px`;
+                } else {
+                    center.style.position = "fixed";
+                    center.style.top = "20px";
+                    center.style.width = "201px";
+                    center.style.height = "196px";
+                    center.style.right = "-9px";
+                }
+            };
+    
+            window.addEventListener("scroll", scrollAnimation);
+            return () => window.removeEventListener("scroll", scrollAnimation);
+        }
     }, []);
     
     return(
@@ -158,38 +175,60 @@ function Invite() {
                 <I.Contents id="contents">
                     <div>
                         <Title className="fade-on-scroll" title="Invitation"/>
-                        <I.Detail className="fade-on-scroll">
-                            졸업은 학부 생활의 마침표인 동시에 대학 그 후의 세상으로 나아감의 의미를 가지고 있습니다.
-                        </I.Detail>
-                        <I.Detail className="fade-on-scroll">
-                            열정을 다해 달려온 학부생활의 마침표를 찍고, 문을 열었을 때 그 앞에 무엇이 펼쳐져 있을지는 알 수 없습니다.
-                        </I.Detail>
-                        <I.Detail className="fade-on-scroll">
-                            다만, 알 수 없는 그 너머의 공간이 우리 모두가 꿈꾸던 찬란한 세상이길 바라며,
-                        </I.Detail>
-                        <I.Detail className="fade-on-scroll">
-                            마침표를 의미하는 세미콜론, 그리고 그 틈새로 비치는 미정의 세계를 보여주는 전시를 기획했습니다.
-                        </I.Detail>
+                        <div  className="fade-on-scroll" style={{ marginBottom: !animation ? "15px" : "40px" }}>
+                            <I.Detail>
+                                창작의 즐거움과 몰입 속에서 자유롭게 표현하고
+                            </I.Detail>
+                            <I.Detail>
+                                도전하는 상상의 공간, 도파민 자유지대
+                            </I.Detail>
+                        </div>
+                        <div className="fade-on-scroll" style={{ marginBottom: !animation ? "15px" : "40px" }}>
+                            <I.Detail>
+                                IT미디어공학도의 세계에서는
+                            </I.Detail>
+                            <I.Detail>
+                                감정이 코드로 흐르고, 상상이 데이터로 변환됩니다.
+                            </I.Detail>
+                        </div>
+                        <div className="fade-on-scroll" style={{ marginBottom: !animation ? "15px" : "40px" }}>
+                            <I.Detail>
+                                기술과 예술이 만나 경계를 허무는 이 공간에서,
+                            </I.Detail>
+                            <I.Detail>
+                                우리는 한계 없는 짜릿함과 설렘의 순간을 경험했습니다.
+                            </I.Detail>
+                            <I.Detail>
+                                그리고 마침내 자유로운 세계를 이루어냈습니다.
+                            </I.Detail>
+                        </div>
+                        <div className="fade-on-scroll" style={{ marginBottom: !animation ? "15px" : "40px" }}>
+                            <I.Detail>
+                                오늘, 우리가 이뤄낸 이 자유지대에서 단순한 자극을 넘어
+                            </I.Detail>
+                            <I.Detail>
+                                그 이상의 즐거움을 발견할 수 있길 바랍니다.
+                            </I.Detail>
+                        </div>
                     </div>
                     <div >
                         <Title className="fade-on-scroll" title="Information"/>
                         <I.SubTitle className="fade-on-scroll">전시 일정</I.SubTitle>
-                        <DateBox />
+                        <DateBox type/>
                     </div>
                     <div>
                         <I.SubTitle className="fade-on-scroll">오시는 길</I.SubTitle>
-                        <MapBox />
+                        <MapBox type={animation}/>
                     </div>
                     <div>
                         <I.SubTitle className="fade-on-scroll">About Us</I.SubTitle>
-                        <AboutUs className="fade-on-scroll"/>
-                    </div>
-                    <div>
-                        <Title className="fade-on-scroll" title="Projects"/>
-                        {/* slick */}
+                        <AboutUs className="fade-on-scroll" type={animation}/>
                     </div>
                 </I.Contents>
+                <Title title="Projects"/>
+                <Slick />
             </I.Invite>
+            <Footer type="invite" />
         </>
     )
 }
