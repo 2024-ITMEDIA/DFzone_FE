76a5a2dbdae844a4034b88e88db6af56e969705a
mogg22 <ahrthdus1@gmail.com>
2024-11-11 14:33:21 +0900

    Fix: 헤더, 푸터 route 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,13 @@ import ProjectDetail from "./pages/ProjectDetail";
 import Map from "./pages/Map";
 import Guest from "./pages/Guest";
 import None from './pages/None';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      {location.pathname !== "/invite" && <Header/>}
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route exact path="/invite" element={<Invite />} />
@@ -24,6 +27,7 @@ function App() {
         <Route exact path="/guestbook" element={<Guest />} />
         <Route exact path="/*" element={<None />} />
       </Routes>
+      {location.pathname !== "/invite" && <Footer type="default" />}
     </div>
   );
 }
