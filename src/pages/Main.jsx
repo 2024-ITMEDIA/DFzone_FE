6e6a01b4b3952bd1469998a809ae3de5512f5ec3
mogg22 <ahrthdus1@gmail.com>
2024-11-13 04:08:07 +0900

    Feat : 메인페이지 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useEffect, useState }from "react";
 import * as M from "../styles/MainStyle";
+import logo from "../img/text_logo.png";
+import date from "../img/text_date.png";
+import center from "../img/img_center.png";
+import flower from "../img/img_flower.png";
+import leaves from "../img/img_leaves.png";
+import light1 from "../img/img_light1.png";
+import light2 from "../img/img_light2.png";
+import rainbow from "../img/img_rainbow.png";
+import spark1 from "../img/img_spark1.png";
+import spark2 from "../img/img_spark2.png";
+import cloud from "../img/img_cloud.png";
+import exhibition from "../img/text_exhibition.png";
+import Button from '../components/Button';
 
 function Main() {
+    const [animate, setAnimate] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setAnimate(true);
+        }, 1500);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <>
-            <M.Main>메인</M.Main>
-        </>
+        <M.Main>
+            <M.Animations>
+                <M.Title>2024 덕성여자대학교 IT미디어공학전공 제12회 졸업전시회</M.Title>
+                <M.Logo src={logo} alt='도파민 자유지대' animate={animate} />
+                <M.Date src={date} alt='2024/11/27-11/29/' animate={animate} />
+                <M.Center src={center} alt='center' animate={animate} />
+                <M.Flower src={flower} alt='flower' animate={animate} />
+                <M.Leaves src={leaves} alt='leaves' animate={animate} />
+                <M.Light1 src={light1} alt='light1' animate={animate} />
+                <M.Light2 src={light2} alt='light2' animate={animate} />
+                <M.Rainbow src={rainbow} alt='rainbow' animate={animate} />
+                <M.Spark1 src={spark1} alt='spark1' animate={animate} />
+                <M.Spark2 src={spark2} alt='spark2' animate={animate} />
+                <M.Cloud src={cloud} alt='cloud' animate={animate} />
+                <M.Exhibition src={exhibition} alt='exhibition' animate={animate} />
+            </M.Animations>
+            <M.ButtonArea animate={animate}>
+                <Button />
+            </M.ButtonArea>
+        </M.Main>
     );
 }
 
--- src/styles/MainStyle.js
+++ src/styles/MainStyle.js
@@ -1,4 +1,251 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import palette from "../lib/colorPalette";
 
-export const Main = styled.div``;
+export const Main = styled.div`
+    height: 100vh;
+`;
+
+export const Animations = styled.div`
+    width: 100vw;
+    position: relative;
+`;
+
+const animationStyles = css`
+    ${({ animate }) => animate && css`
+        transform: translate(0, 0);
+        transition: all 1s ease;
+    `}
+`;
+
+export const Title = styled.div`
+    width: 300px;
+    height: 22px;
+    display: flex;
+    align-items: center;
+    color: ${palette.color3};
+    text-align: center;
+    font-feature-settings: 'liga' off, 'clig' off;
+    font-family: "S-Core Dream";
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 200;
+    line-height: 22px;
+    position: absolute;
+    z-index: 10;
+    top: 46px;
+    left: 35px;
+    @media (hover: hover) and (pointer: fine) {
+        width: 520px;
+        font-size: 20px;
+        position: absolute;
+        top: 74px;
+        left: 57px;
+    }
+`;
+
+export const Logo = styled.img`
+    width: ${({ animate }) => (animate ? "233px" : "306px")};
+    height: ${({ animate }) => (animate ? "134px" : "176px")};
+    position: absolute;
+    z-index: 10;
+    top: ${({ animate }) => (animate ? "66px" : "68px")};
+    left: 30px;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 673px;
+        height: 387px;
+        position: absolute;
+        top: 96px;
+        left: 57px;
+    }
+`;
+
+export const Date = styled.img`
+    width: 131px;
+    height: 52px;
+    position: absolute;
+    z-index: 10;
+    top: 253px;
+    left: 30px;
+    opacity: ${({ animate }) => (animate ? 0 : 1)};
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 288px;
+        height: 114px;
+        position: absolute;
+        top: 514px;
+        left: 57px;
+    }
+`;
+
+export const Center = styled.img`
+    width: 332px;
+    height: 324px;
+    position: absolute;
+    z-index: 8;
+    top: ${({ animate }) => (animate ? "266px" : "411px")};
+    right: 34px;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 730px;
+        height: 712px;
+        position: absolute;
+        top: 290px;
+        right: 77px;
+    }
+`;
+
+
+export const Flower = styled.img`
+    width: 55px;
+    height: 49px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "291px" : "470px")};
+    left: ${({ animate }) => (animate ? "9px" : "10px")};
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 121px;
+        height: 108px;
+        top: 555px;
+        left: 495px;
+    }
+`;
+
+export const Leaves = styled.img`
+    width: 91px;
+    height: 94px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "221px" : "350px")};
+    right: 0;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 200px;
+        height: 207px;
+        top: 78px;
+        right: 0;
+    }
+`;
+
+export const Light1 = styled.img`
+    width: 89px;
+    height: 81px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "53px" : "232px")};
+    right: 43px;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 195px;
+        height: 178px;
+        top: 62px;
+        right: 556px;
+    }
+`;
+
+export const Light2 = styled.img`
+    width: 66px;
+    height: 50px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "203px" : "325px")};
+    left: 15px;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 145px;
+        height: 110px;
+        top: 682px;
+        left: 22px;
+    }
+`;
+
+export const Rainbow = styled.img`
+    width: 113px;
+    height: 71px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "213px" : "342px")};
+    left: 70px;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 249px;
+        height: 156px;
+        top: 700px;
+        left: 246px;
+    }
+`;
+
+export const Spark1 = styled.img`
+    width: 51px;
+    height: 61px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "106px" : "273px")};
+    right: 10px;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 112px;
+        height: 134px;
+        top: 151px;
+        right: 434px;
+    }
+`;
+
+export const Spark2 = styled.img`
+    width: 51px;
+    height: 54px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "529px" : "708px")};
+    left: 22px;
+    @media (hover: hover) and (pointer: fine) {
+        width: 112px;
+        height: 119px;
+        top: 883px;
+        left: 74px;
+    }
+`;
+
+export const Cloud = styled.img`
+    width: 104px;
+    height: 54px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "190px" : "323px")};
+    right: 80px;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 229px;
+        height: 119px;
+        top: 862px;
+        left: 460px;
+    }
+`;
+
+export const Exhibition = styled.img`
+    width: 29px;
+    height: 224px;
+    position: absolute;
+    z-index: 5;
+    top: ${({ animate }) => (animate ? "372px" : "600px")};
+    right: 10px;
+    ${animationStyles}
+    @media (hover: hover) and (pointer: fine) {
+        width: 64px;
+        height: 492px;
+        top: 530px;
+        right: 26px;
+    }
+`;
+
+export const ButtonArea = styled.div`
+    width: 100vw;
+    position: absolute;
+    bottom: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: ${({ animate }) => (animate ? 1 : 0)};
+    ${animationStyles}
+`;
