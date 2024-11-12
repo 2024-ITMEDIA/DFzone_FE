387099fa5b91be80d1a3998e82c7244f1a23bf94
mogg22 <ahrthdus1@gmail.com>
2024-11-13 04:14:40 +0900

    Fix : 버튼 컴포넌트 클릭 이벤트 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import * as B from "../styles/components/ButtonStyle";
 import styled from "styled-components";
 
@@ -7,22 +8,26 @@ const ButtonRow = B.ButtonRow;
 const ButtonTitle = B.ButtonTitle;
 
 function Button() {
+    const navigate = useNavigate();
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
     return (
         <>
             <B.ButtonContainer>
                 <B.ButtonRow>
-                    <B.Button>
+                    <B.Button onClick={() => handleNavigation('/info')}>
                     <ButtonTitle>Information</ButtonTitle>
                     </B.Button>
-                    <B.Button>
+                    <B.Button onClick={() => handleNavigation('/project')}>
                         <ButtonTitle>Projects</ButtonTitle>
                     </B.Button>
                 </B.ButtonRow>
                 <B.ButtonRow>
-                    <B.Button>
+                    <B.Button onClick={() => handleNavigation('/map')}>
                         <ButtonTitle>Map</ButtonTitle>
                     </B.Button>
-                    <B.Button>
+                    <B.Button onClick={() => handleNavigation('/guestbook')}>
                         <ButtonTitle>Guestbook</ButtonTitle>
                     </B.Button>
                 </B.ButtonRow>
