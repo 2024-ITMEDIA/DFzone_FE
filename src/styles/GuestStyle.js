38b50551db9ab198b074b02aa1a46060faa092fd
Yeonwoo <lxr4817@gmail.com>
2024-11-12 23:42:01 +0900

    Feat : "TextField 구현 및 Guest 중간 구현

--- src/styles/GuestStyle.js
+++ src/styles/GuestStyle.js
@@ -2,3 +2,31 @@ import styled from "styled-components";
 import palette from "../lib/colorPalette";
 
 export const Guest = styled.div``;
+
+export const GuestContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Title = styled.div`
+    width: 100%;
+    margin-top: 42px;
+    color: var(--Color3, #6A226D);
+    text-align: center;
+    font-feature-settings: 'liga' off, 'clig' off;
+    font-family: "S-Core Dream";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 22px; /* 110% */
+`;
+
+export const Line = styled.div`
+    width: 40px;
+    height: 1.5px;
+    background: var(--Color3, #6A226D); /* 선의 색상 */
+    margin-top: 17px;
+    margin-bottom: 32px;
+`;
